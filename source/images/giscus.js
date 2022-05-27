c0cb63588c9038473f9058214b24971a7ca4ac8b
xyz8848 <xyz8848git@outlook.com>
2022-05-26 21:56:55 -0500

    Create giscus.js

---
+++ source/images/giscus.js
@@ -0,0 +1,14 @@
+<script src="https://giscus.app/client.js"
+        data-repo="xyz8848/Blog"
+        data-repo-id="R_kgDOHHONxA"
+        data-category="General"
+        data-category-id="DIC_kwDOHHONxM4CPUS_"
+        data-mapping="pathname"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="light"
+        data-lang="zh-CN"
+        crossorigin="anonymous"
+        async>
+</script>
